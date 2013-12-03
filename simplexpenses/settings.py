2739e0146a682d18d61d9b47a62b2a4bee88aa08
Dimitar Ralev <dim.ralev@gmail.com>
2013-12-03 19:19:25 +0200

    1

--- simplexpenses/settings.py
+++ simplexpenses/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = 'fc0m$38h=bhjr!bhz^0wyr2b%v4%((ne+l5_tr!za#wwmhwehq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 TEMPLATE_DEBUG = True
 
