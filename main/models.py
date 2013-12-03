01e1309c62ebfe6c15541f6d64404c872f09fb0b
Dimitar Ralev <dim.ralev@gmail.com>
2013-12-03 23:00:38 +0200

    03.12.13

--- main/models.py
+++ main/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+# Create your models here.
 class Category(models.Model):
 	title = models.CharField()
 	description = models.CharField()
