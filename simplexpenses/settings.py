89135990922ddbdbbc8e6d8ffab1023b91749a32
Dimitar Ralev <dim.ralev@gmail.com>
2013-12-03 18:24:48 +0200

    1

--- simplexpenses/settings.py
+++ simplexpenses/settings.py
@@ -81,6 +81,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+STATIC_ROOT = ''
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
@@ -99,16 +100,9 @@ ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
 import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = ''
-STATIC_URL = '/static/'
+#BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+#)
 
-STATICFILES_FINDERS = (
-                       'django.contrib.staticfiles.finders.FileSystemFinder',
-                       'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-                       #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-                       )
