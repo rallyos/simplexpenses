b22ea125456766b536dd202515c92b9d6fdf3046
Dimitar Ralev <dim.ralev@gmail.com>
2013-12-03 20:29:55 +0200

    1

--- main/views.py
+++ main/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
-def home(request):
+def index(request):
 	return render(request, 'index.html')
--- simplexpenses/urls.py
+++ simplexpenses/urls.py
@@ -1,12 +1,8 @@
 from django.conf.urls import patterns, include, url
-from main import views
 from django.contrib import admin
-admin.autodiscover()
+#admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', 'main.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', include('main.urls'))
+#    url(r'^admin/', include(admin.site.urls)),
 )
