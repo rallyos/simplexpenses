b10abcec07b75e75069a2c3195b2e57272df0ab7
Dimitar Ralev <dim.ralev@gmail.com>
2013-12-05 00:18:27 +0200

    04.12.13

--- main/serializers.py
+++ main/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-
+from main import models
 
 class ExpenseSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
