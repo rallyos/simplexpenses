12c0988de16ea060573dcbe6dfc9c81555c3b10d
Dimitar Ralev <dim.ralev@gmail.com>
2013-12-04 14:04:13 +0200

    04.12.13

--- main/models.py
+++ main/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from tastypie.utils.timezone import now
+from django.template.defaultfilters import slugify
 
 
 class Category(models.Model):
@@ -8,6 +10,15 @@ class Category(models.Model):
 	description = models.CharField(max_length=80)
 	color = models.CharField(max_length=7)
 
+    def __unicode__(self):
+        return self.title
+    def save(self, *args, **kwargs):
+        # For automatic slug generation.
+        if not self.slug:
+            self.slug = slugify(self.title)[:50]
+
+        return super(Entry, self).save(*args, **kwargs)
+
 class Expense(models.Model):
 	user = models.ForeignKey(User)
 	amount = models.DecimalField(max_digits=12, decimal_places=2)
@@ -15,8 +26,33 @@ class Expense(models.Model):
 	category = models.ForeignKey(Category)
 	date = models.DateField(auto_now_add=True)
 
+    def __unicode__(self):
+        return self.title
+    def save(self, *args, **kwargs):
+        # For automatic slug generation.
+        if not self.slug:
+            self.slug = slugify(self.title)[:50]
+
+        return super(Entry, self).save(*args, **kwargs)    def save(self, *args, **kwargs):
+        # For automatic slug generation.
+        if not self.slug:
+            self.slug = slugify(self.title)[:50]
+
+        return super(Entry, self).save(*args, **kwargs)
+
 class Planned(models.Model):
 	user = models.ForeignKey(User)
 	cateogory = models.ForeignKey(Category)
 	planned_amount = models.DecimalField(max_digits=12, decimal_places=2)
 	planned_month = models.DateField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        # For automatic slug generation.
+        if not self.slug:
+            self.slug = slugify(self.title)[:50]
+
+        return super(Entry, self).save(*args, **kwargs)
+        
--- simplexpenses/urls.py
+++ simplexpenses/urls.py
@@ -2,16 +2,13 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 from tastypie.api import Api
-from main.api.resources import ExpenseResource
+from main.api import ExpenseResource
 
 #admin.autodiscover()
-
-v1_api = Api(api_name='v1')
-v1_api.register(ExpenseResource())
-
+expense_resource = ExpenseResource()
 
 urlpatterns = patterns('',
     url(r'^$', include('main.urls')),
-	url(r'^api/', include(v1_api.urls)),
+	url(r'^api/', include(expense_resource.urls)),
 #    url(r'^admin/', include(admin.site.urls)),
 )
