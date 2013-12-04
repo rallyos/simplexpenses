5f151e2e85eff96089a8fd4061d0a8003c8f9b2c
Dimitar Ralev <dim.ralev@gmail.com>
2013-12-04 15:45:55 +0200

    04.12.13

--- main/api.py
+++ main/api.py
@@ -7,19 +7,19 @@ from main.models import Category, Expense, Planned
 class ExpenseResource(ModelResource):
     class Meta:
         queryset = Expense.objects.all()
-        #allowed_methods = ['get']
+        allowed_methods = ['get', 'post', 'put']
         authorization = Authorization()
 
 
 class CategoryResource(ModelResource):
     class Meta:
         queryset = Category.objects.all()
-        #allowed_methods = ['get']
+        allowed_methods = ['get', 'post', 'put']
         authorization = Authorization()
 
 
 class PlannedResource(ModelResource):
     class Meta:
         queryset = Planned.objects.all()
-        #allowed_methods = ['get']
+        allowed_methods = ['get', 'post', 'put']
         authorization = Authorization()
