3032fdc4b30135b4a9989eaa4d7737713538bee4
Dimitar Ralev <dim.ralev@gmail.com>
2013-12-05 00:11:18 +0200

    04.12.13

--- simplexpenses/urls.py
+++ simplexpenses/urls.py
@@ -5,16 +5,6 @@ from rest_framework import viewsets, routers
 
 #admin.autodiscover()
 
-# ViewSets define the view behavior.
-class ExpenseViewSet(viewsets.ModelViewSet):
-    model = Expense
-
-class CategoryViewset(viewsets.ModelViewSet):
-    model = Category
-
-class PlannedViewset(viewsets.ModelViewSet):
-    model = Planned
-
 # Routers provide an easy way of automatically determining the URL conf
 router = routers.DefaultRouter()
 router.register(r'expense', views.ExpenseViewSet)
@@ -26,7 +16,7 @@ router.register(r'planned', views.PlannedViewset)
 urlpatterns = patterns('',
     #url(r'^$', include('main.urls')),
 
-    url(r'^', include(router.urls)),
+    url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 #    url(r'^admin/', include(admin.site.urls)),
 )
