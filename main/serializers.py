b7feb9cb3c831e38003ee33663f662464a0ee3aa
Dimitar Ralev <dim.ralev@gmail.com>
2013-12-05 00:32:06 +0200

    04.12.13

--- main/serializers.py
+++ main/serializers.py
@@ -3,16 +3,25 @@ from rest_framework import serializers
 from main.models import Expense, Category, Planned
 
 class ExpenseSerializer(serializers.HyperlinkedModelSerializer):
+
+  	user_id = serializers.IntegerField(required=False)
+
     class Meta:
         model = Expense
         fields = ('user_id', 'amount', 'description', 'category_id', 'date')
 
 class CategorySerializer(serializers.HyperlinkedModelSerializer):
+
+  	user_id = serializers.IntegerField(required=False)
+
     class Meta:
         model = Category
         fields = ('user_id', 'title', 'description', 'color')
 
 class PlannedSerializer(serializers.HyperlinkedModelSerializer):
+
+  	user_id = serializers.IntegerField(required=False)
+
     class Meta:
         model = Planned
         fields = ('user_id', 'category_id', 'planned_amount', 'planned_month')
