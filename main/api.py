5e1a522778f96fee17fe971a90cd136b6d436a3a
Dimitar Ralev <dim.ralev@gmail.com>
2013-12-04 18:34:38 +0200

    04.12.13

--- main/api.py
+++ main/api.py
@@ -1,5 +1,6 @@
 from tastypie.authorization import Authorization
 from django.contrib.auth.models import User
+from tastypie import fields
 
 from tastypie.resources import ModelResource
 from main.models import Category, Expense, Planned
