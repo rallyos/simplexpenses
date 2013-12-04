725a4b1b7762bf7e982a67a3ab1bf7e89bc2799d
Dimitar Ralev <dim.ralev@gmail.com>
2013-12-04 18:39:41 +0200

    04.12.13

--- simplexpenses/urls.py
+++ simplexpenses/urls.py
@@ -2,10 +2,11 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 from tastypie.api import Api
-from main.api import ExpenseResource, CategoryResource, PlannedResource
+from main.api import ExpenseResource, CategoryResource, PlannedResource, UserResource
 
 #admin.autodiscover()
 v1_api = Api(api_name='v1')
+v1_api.register(UserResource())
 v1_api.register(ExpenseResource())
 v1_api.register(CategoryResource())
 v1_api.register(PlannedResource())
