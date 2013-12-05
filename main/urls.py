a35e035bff38c895979968f3e41b7bce047eaf90
Dimitar Ralev <dim.ralev@gmail.com>
2013-12-05 14:13:58 +0200

    04.12.13

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,9 @@ from django.conf.urls import patterns, url
 from main import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-    url(r'^/user$', views.user, name='user')
+    url(r'^', views.index, name='index'),
+    url(r'^user', views.user, name='user')
+    url(r'^api/', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+#    url(r'^admin/', include(admin.site.urls)),    
 )
