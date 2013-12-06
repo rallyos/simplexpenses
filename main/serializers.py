150e869400e3aa1eb1d2ad9a4209e549bd38b571
Dimitar Ralev <dim.ralev@gmail.com>
2013-12-06 15:01:24 +0200

    04.12.13

--- main/serializers.py
+++ main/serializers.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import User
+#from django.contrib.auth.models import User
 from rest_framework import serializers
 from main.models import Expense, Category, Planned
 
