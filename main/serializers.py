a3da5b724c6c81bed5dffd3ec05f4c2601681738
Dimitar Ralev <dim.ralev@gmail.com>
2013-12-06 11:18:06 +0200

    04.12.13

--- main/serializers.py
+++ main/serializers.py
@@ -2,27 +2,27 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 from main.models import Expense, Category, Planned
 
-class ExpenseSerializer(serializers.ModelSerializer):
+class ExpenseSerializer(serializers.HyperlinkedModelSerializer):
 
-#	user_id = serializers.IntegerField(required=False)
-#  	category_id = serializers.IntegerField(required=False)
+	user_id = serializers.IntegerField(required=False)
+  	category_id = serializers.IntegerField(required=False)
 
 	class Meta:
 		model = Expense
 		fields = ('id', 'user_id', 'amount', 'description', 'category_id', 'date')
 
-class CategorySerializer(serializers.ModelSerializer):
+class CategorySerializer(serializers.HyperlinkedModelSerializer):
 
-#  	user_id = serializers.IntegerField(required=False)
+  	user_id = serializers.IntegerField(required=False)
 
 	class Meta:
 		model = Category
 		fields = ('id', 'user_id', 'title', 'description', 'color')
 
-class PlannedSerializer(serializers.ModelSerializer):
+class PlannedSerializer(serializers.HyperlinkedModelSerializer):
 
-#  	user_id = serializers.IntegerField(required=False)
-#  	category_id = serializers.IntegerField(required=False)
+  	user_id = serializers.IntegerField(required=False)
+  	category_id = serializers.IntegerField(required=False)
 
 	class Meta:
 		model = Planned
