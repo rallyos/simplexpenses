59f8abfb3090117b5128ea7191286c6457c7210b
Dimitar Ralev <dim.ralev@gmail.com>
2013-12-07 16:11:46 +0200

    04.12.13

--- simplexpenses/urls.py
+++ simplexpenses/urls.py
@@ -7,16 +7,16 @@ from main import views
 #admin.autodiscover()
 
 # Routers provide an easy way of automatically determining the URL conf
-router = routers.DefaultRouter()
-router.register(r'expense', views.ExpenseViewSet)
-router.register(r'category', views.CategoryViewSet)
-router.register(r'planned', views.PlannedViewSet)
+api = routers.DefaultRouter()
+api.register(r'expense', views.ExpenseViewSet)
+api.register(r'category', views.CategoryViewSet)
+api.register(r'planned', views.PlannedViewSet)
 
 
 
 urlpatterns = patterns('',
     url(r'^', include('main.urls')),
-    url(r'^api/', include(router.urls)),
+    url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 #    url(r'^admin/', include(admin.site.urls)),
 )
