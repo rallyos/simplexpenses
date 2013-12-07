162b08794950e8d9742ce3a3849140ad94834747
Dimitar Ralev <dim.ralev@gmail.com>
2013-12-07 19:52:06 +0200

    04.12.13

--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from main.models import Expense, Category, Planned
 from django.contrib.auth import authenticate, login, logout
 
