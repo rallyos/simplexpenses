457d0ddc317687fdca110438eb23f4aa6032eb0d
Dimitar Ralev <dim.ralev@gmail.com>
2013-12-07 19:53:40 +0200

    04.12.13

--- main/views.py
+++ main/views.py
@@ -33,6 +33,9 @@ def login_user(request):
         # Return an 'invalid login' error message.
             return HttpResponse(status=404)
 
+def logout_user(request):
+    logout(request)
+    return redirect('/')
 
 
 class ExpenseViewSet(viewsets.ModelViewSet):
