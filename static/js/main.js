8b814ccf85fd251d8882bc01e504bc563094436a
Dimitar Ralev <dim.ralev@gmail.com>
2013-12-09 12:40:54 +0200

    04.12.13

--- static/js/main.js
+++ static/js/main.js
@@ -18,7 +18,7 @@ expensesApp.controller('CategoriesList', function ($scope, $http) {
 	});
 
 	$scope.selectCategory = function() {
-
+		this.categoryClass = !this.categoryClass;
 		$scope.categoryClass = !$scope.categoryClass;
 	}
 	$scope.translateForm = function() {
