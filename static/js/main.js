3a41cba158c43017cec0e5152138b76544ad01be
Dimitar Ralev <dim.ralev@gmail.com>
2013-12-09 15:58:01 +0200

    04.12.13

--- static/js/main.js
+++ static/js/main.js
@@ -3,13 +3,13 @@ var expensesApp = angular.module('expenses', []);
 
 
 
-expensesApp.controller('ExpensesGraph', function($scope, Expenses) {
+expensesApp.controller('ExpensesGraph', function($scope) {
 	$scope.testing = 8
 });
 
-expensesApp.controller('ExpensesList', function ($scope, $resource) {
-	var Expenses = $resource('/api/expenses/');
-	$scope.data = Expenses.get();
+expensesApp.controller('ExpensesList', function ($scope) {
+	//var Expenses = $resource('/api/expenses/');
+	//$scope.data = Expenses.get();
 });
 
 
