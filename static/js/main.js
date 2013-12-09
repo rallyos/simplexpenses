c8f458922ea346113f9db36f2f5c34ad651cd241
Dimitar Ralev <dim.ralev@gmail.com>
2013-12-09 14:52:42 +0200

    04.12.13

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,22 @@
-var expensesApp = angular.module('test-module', []);
+var expensesApp = angular.module('expenses', []);
+
+
+expensesApp.factory('Expenses', function($http) {
+	$http.get('api/expense/').success(function(data) {
+		$scope.expenses = data;
+		console.log('wtd')
+	});
+})
+
 
 expensesApp.controller('ExpensesGraph', function($scope) {
 	$scope.testing = 8
-})
+});
 
-expensesApp.controller('ExpensesList', function ($scope, $http) {
-  $http.get('api/expense/').success(function(data) {
-    $scope.expenses = data;
-  });
+expensesApp.controller('ExpensesList', function ($scope, $http, ) {
+	$http.get('api/expense/').success(function(data) {
+		$scope.expenses = data;
+	});
 });
 
 
@@ -18,7 +27,6 @@ expensesApp.controller('CategoriesList', function ($scope, $http) {
 	});
 
 	$scope.selectCategory = function() {
-		console.log('select')
 		this.categoryClass = !this.categoryClass;
 	}
 
@@ -27,67 +35,3 @@ expensesApp.controller('CategoriesList', function ($scope, $http) {
 	}
 
 });
-
-
-
-
-// JUST TESTING
-
-var add_expense_button = document.getElementsByClassName('add-expense')[0]
-
-var expense_form = document.getElementsByClassName('test')[0]
-var expense_amount = document.getElementsByClassName('add-expense-sum')[0]
-
-add_expense_button.addEventListener("click", function () {
-	var test = this.textContent
-
-	expense_amount.focus()
-
-	if (test == '^') {
-		this.textContent = '+';
-	} else {
-		this.textContent = '^';
-	}
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-$('.add-expense-category').on('click', function() {
-	$('.add-expense-category').removeClass('add-expense-category-selected')
-	$(this).toggleClass('add-expense-category-selected')
-})
-
-$('.menu-toggle').on('click', function() {
-	$('.menu-links').toggle()
-})
-
-$('.add-expense-submit').on('click', function() {
-	$('.sum-expenses').text('14.33')
-	$('.yellow').height('287')
-})
-*/
-
