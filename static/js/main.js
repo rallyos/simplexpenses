9e8e5a2d30562ce94bab08875b0561dfbd98ad77
Dimitar Ralev <dim.ralev@gmail.com>
2013-12-09 19:29:19 +0200

    04.12.13

--- static/js/main.js
+++ static/js/main.js
@@ -1,25 +1,60 @@
 var expensesApp = angular.module('expenses', ['ngResource']);
 
 
-expensesApp.factory('expensesData', function($http) {
+expensesApp.factory('appData', function($http) {
 
-	var myService = {
+	var data = {}
+
+	var data.danni = {
 		
 		testu: function() {
 
-			var promise = $http.get('/api/expense/').then(function (response) {
+			var fetchedData = {}
+
+			var fetchedData.expenses = $http.get('/api/expense/').then(function (response) {
 				console.log(response);
 				return response.data;
 			});
 
+			var fetchedData.categories = $http.get('/api/category/').then(function (response) {
+				console.log(response);
+				return response.data;
+			});
+
+			console.log(fetchedData)
 			// Return the promise to the controller
-			return promise;
+			return fetchedData;
 		}
 	};
 
-  return myService;
+	console.log(data)
+  return data;
 });
 
+
+expensesApp.controller('mainController', function($scope, appData) {
+
+	categoriesData.testu().then(function(d) {
+		console.log(d)
+
+		//$scope.expenses = d;
+		//$scope.categories = c;
+	});
+
+	$scope.selectCategory = function() {
+		this.categoryClass = !this.categoryClass;
+	}
+
+	$scope.translateForm = function() {
+		$scope.headerClass = !$scope.headerClass;
+	}
+
+});
+
+
+
+
+/*
 expensesApp.factory('categoriesData', function($http) {
 
 	var myService = {
@@ -38,35 +73,7 @@ expensesApp.factory('categoriesData', function($http) {
 
   return myService;
 });
-
-expensesApp.controller('mainController', function($scope, expensesData, categoriesData) {
-	
-	categoriesData.testu().then(function(c) {
-		$scope.categories = c;
-	});
-	expensesData.testu().then(function(d) {
-		$scope.expenses = d;
-	});
-
-	$scope.selectCategory = function() {
-		this.categoryClass = !this.categoryClass;
-	}
-
-	$scope.translateForm = function() {
-		$scope.headerClass = !$scope.headerClass;
-	}
-
-});
-
-
-
-
-
-
-
-
-
-
+*/
 
 
 
