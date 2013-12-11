e20f44bd198579294302a5460dd5ac0bae749c28
Dimitar Ralev <dim.ralev@gmail.com>
2013-12-12 00:12:11 +0200

    11.12.13

--- static/js/main.js
+++ static/js/main.js
@@ -1,31 +1,55 @@
 var expensesApp = angular.module('expenses', ['ngResource', 'ngAnimate']);
 
 
-
-function getCookie (sKey) {
-    return decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null;
-}  
-
-csrftoken = getCookie('csrftoken')
-
-
 expensesApp.config(function($httpProvider) {
 	$httpProvider.defaults.headers.post = {'X-CSRFToken': csrftoken, 'Content-Type': 'application/json'}
 })
 
-expensesApp.factory('Expenses', ['$resource', function($resource, $httpProvider) {
+expensesApp.factory('Expense', ['$resource', function($resource) {
 	return $resource( '/api/expense/');
 }]);
 
-expensesApp.controller('mainController', function($scope, Expenses) {
+expensesApp.factory('Category', ['$resource', function($resource) {
+	return $resource( '/api/category/');
+}]);
+
+expensesApp.controller('mainController', function($scope, Expense, Category) {
 
 	$scope.testam = function(cat_id) {
 
 		description = 'from the form'
-		Expenses.save({'amount': $scope.exp_amount, 'description': $scope.exp_description, 'category_id': cat_id},function(response) {
+		Expense.save({'amount': $scope.exp_amount, 'description': $scope.exp_description, 'category_id': cat_id},function(response) {
 			$scope.expenses.unshift(response);
+			$scope.sumExpenses()
 		});
 	}
+
+	$scope.testamdve = function(event) {
+		$scope.categories.push({'title': 'Testing', 'description': 'a new category', 'color': '#6FC28E'});
+		lastCategory = event.target.previousElementSibling
+		lastCategory.setAttribute('contenteditable', 'true')
+		lastCategory.focus()
+	}
+
+	$scope.dsa = function() {
+		// highly expiremental
+		this_category = this.category.id
+		sum = 0
+ 		for (i=0;$scope.expenses.length > i;i++) {
+			if ($scope.expenses[i].category_id == this_category) {
+				var number = Number($scope.expenses[i].amount)
+				sum = sum + number
+				var amount = sum.toFixed(2);
+			} else {
+				continue
+			}
+		}
+		amount = amount / $scope.thisMonthAmount * 100
+		amount = amount.toFixed(2)
+		return amount + '%'
+	}
+
+
 	$scope.expenses = expenses
 	$scope.categories = categories
 
@@ -69,6 +93,12 @@ expensesApp.controller('mainController', function($scope, Expenses) {
 
 });
 
+
+function getCookie (sKey) {
+    return decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null;
+}  
+
+csrftoken = getCookie('csrftoken')
 
 
 
