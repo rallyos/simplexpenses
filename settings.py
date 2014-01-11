9546811ad334a2b55ff139efefb3a25741f58383
Dimitar Ralev <dim.ralev@gmail.com>
2014-01-11 10:14:51 +0200

    11.01.14

--- settings.py
+++ settings.py
@@ -120,7 +120,8 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': (
        'rest_framework.permissions.AllowAny',
-    )
+    ),
+    
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
